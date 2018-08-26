8290ee6301087eedf6b9deb1e1bf777d86e3afe6
Dev Singh <devsingh@Devs-MacBook-Pro.local>
2018-08-26 13:20:00 -0700

    first commit with the folder structures and some operations

---
+++ App/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+// Requiring the core library to get the Model
+require_once 'libraries/core.php';
+
+// Requiring the database library to get the database
+require_once 'libraries/database.php';
+
+// Requiring the controller library to get the controller
+require_once 'libraries/controller.php';
+?>
---
+++ App/libraries/core.php
@@ -0,0 +1,76 @@
+<?php
+/*
+    - This is the core library that will be used to get the URL 
+    - and load the current view
+*/
+class core{
+    protected $currentController = 'pages';
+    protected $currentMethod = 'index';
+
+    public function __construct(){
+        $url = $this->getUrl();
+        /*
+            - This will see if the requested file exists if not then 
+            - the fallback is the pages class and index method
+            - ucwords will capatlize the first word in the url
+            - uset will set the array index to null
+        */
+        if(file_exists('../App/controllers/' . ucwords($url[0]) . '.php')){
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }
+        /*
+            - This will require the correct controller from the 
+            - controllers folder
+        */
+        require_once '../App/controllers/' . $this->currentController . '.php';
+        /* 
+            - This will initilize the current controller 
+            - For eg : if the url is /page/add
+            - It will initilize $page = new page;
+        */
+        $this->currentController = new $this->currentController;
+        /*
+            - This will check if the url has another index required 
+            - then it will check if the method exists if yes then 
+            - set the currentMethod 
+            - to the required one
+        */
+        if(isset($url[1])){
+            if(method_exists($this->currentController,$url[1])){
+                $this->currentMethod = $url[1];
+                unset($url[1]);
+            }
+        }
+        /*
+            - If the url still has some values then add it 
+            - to params otherwise 
+            - store it as an empty array
+        */
+        $this->params = $url ? array_values($url) : [];
+
+
+    }
+/*
+    - getUrl method will take URL and return an array of it 
+    - ex: MVC/dev/singh
+    - will return Array( [0] => dev [1] => singh )
+*/
+    public function getUrl(){
+        if(isset($_GET['url'])){
+            // Trim White Space
+            $url = rtrim($_GET['url'],'/');
+
+            // filter_var will remove everthing from the
+            // URL that is not suppossed to be there
+            $url = filter_var($url,FILTER_SANITIZE_URL);
+
+            // This will convert the $url into an array
+            $url = explode('/', $url);
+            return $url;
+        } 
+    }
+
+}
+
+?>
